add65dd0ca3ee8eb8404b92246afe63524c3bbb8
PHENGSIAROUN Dany <dphengsiaroun@alliance.net>
2018-10-04 11:53:57 +0200

    add: class active

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,9 +11,7 @@ export class SidebarComponent implements OnInit {
 
   currentUrl: string;
 
-  constructor(private router: Router) {
-    router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
-  }
+  constructor(private router: Router) {}
 
   ngOnInit() {
   }
